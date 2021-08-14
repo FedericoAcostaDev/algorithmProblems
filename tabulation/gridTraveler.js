e1febd33fd47690c088607846fc41d242a0f70e5
FedericoAcostaDev <fedeacos045@gmail.com>
2021-08-14 12:45:02 -0300

    tabulation

---
+++ tabulation/gridTraveler.js
@@ -0,0 +1,9 @@
+
+const gridTraveler = (m, n) => {
+    const table = Array (m + 1)
+    .fill()
+    .map(() => Array (n + 1));
+};
+
+console.log(gridTraveler(3, 3));
+console.log(gridTraveler(6, 6));
