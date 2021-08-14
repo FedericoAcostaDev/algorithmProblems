0c621204f80ab5d246464565ec03638ad963e479
FedericoAcostaDev <fedeacos045@gmail.com>
2021-08-14 19:53:09 -0300

    finished

---
+++ tabulation/countConstruct.js
@@ -0,0 +1,18 @@
+
+
+const countConstruct = (target, worldBanK) => {
+    const table = Array(target.length + 1).fill(0);
+    table[0] = 1;
+
+    for(let i = 0; i <= target.length; i++) {
+        for ( let word of worldBanK) {
+            if (target.slice(i, i + word.length) === word) {
+                table[i + word.length] += table[i];
+            }
+        }
+    }
+    return table[target.length];
+
+};
+
+console.log(countConstruct('purple',['purp', 'p' ,'ur', 'le', 'purpl'])); //2
