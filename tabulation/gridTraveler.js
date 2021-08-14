271e7a7da8cc258972fa8cfede19070dd868540c
FedericoAcostaDev <fedeacos045@gmail.com>
2021-08-14 14:31:20 -0300

    cansum

--- tabulation/gridTraveler.js
+++ tabulation/gridTraveler.js
@@ -2,7 +2,19 @@
 const gridTraveler = (m, n) => {
     const table = Array (m + 1)
     .fill()
-    .map(() => Array (n + 1));
+    .map(() => Array (n + 1).fill(0));
+    
+    table[1][1] = 1;
+
+    for( let i = 0; i <= m; i++){
+        for( let j= 0; j <= m; j++) {
+            const current = table[i][j];
+            if (j + 1 <= n) table[i][j + 1] += current;
+            if (i + 1 <= m) table[i + 1][j] += current;
+        }
+    }
+
+    return table[m][n];
 };
 
 console.log(gridTraveler(3, 3));
